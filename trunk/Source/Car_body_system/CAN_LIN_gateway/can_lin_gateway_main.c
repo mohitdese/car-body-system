8811976383290cfbc71d20f725d99f5e497654cb
sapi35 <sapi35@f2116a12-c552-11de-8df7-59987fa32379>
2009-12-02 22:41:10 +0000

    can_lin_gateway_main modul CAN slave és LIN master modul ver. 2.0: CAN slave, LIN master modulok megvalósítása, ha a hátsó ajtók valamelyikének szól az üzenet, akkor az ennek megfelelő LIN slave modulnak továbbítja az üzenetet.

--- trunk/Source/Car_body_system/CAN_LIN_gateway/can_lin_gateway_main.c
+++ trunk/Source/Car_body_system/CAN_LIN_gateway/can_lin_gateway_main.c
@@ -7,7 +7,7 @@
 **             Copyright (c) 2003, 2004 - Atmel Corporation
 **             Proprietary Information
 **
-** Project    	: AVR LIN MASTER EXAMPLE
+** Project    	: AVR CAN SLAVE - LIN MASTER
 ** Module     	: main.C
 ** Description	: main application
 **                Compatible with LIN Specifications 1.3
@@ -15,7 +15,7 @@
 **
 **
 ** Version :     Date:         Author:      Comment:
-**    1.0        19.01.2004    E.G.          Creation
+**    2.0        25.11.2009    Team
 **
 **
 **
@@ -35,6 +35,8 @@
 /*_____ I N C L U D E S ____________________________________________________*/
 /*__________________________________________________________________________*/
 #include "config.h"
+#include "lib_mcu/lin_uart/master_lin.h"
+#include "lib_mcu/lin_uart/lin_lib.h"
 #include "avr/io.h"
 #include <avr/interrupt.h>
 #include "platform.h"
@@ -42,6 +44,8 @@
 #include "dpy_trm_s01.h"
 #include "mcp2515.h"
 
+U8 Buf_SET_SLAVE [4];
+U8 Buf_SET_SLAVE2 [4];
 U8 active;
 
 /* ************************************************************************** */
@@ -51,29 +55,74 @@ U8 active;
 ** ------------------------------------------------------------------------------
 */
 int main (void) {
-  
+  t_frame MESS_SET_SLAVE;
+  t_frame MESS_SET_SLAVE2;
   CAN_message p_message;
+  U8 number_of_frame ;
+  U8 lin_message;
 
-  dpy_trm_s01__Init();
+  //SET SLAVE FRAME
+  MESS_SET_SLAVE.frame_id    = 0x00 ;
+  MESS_SET_SLAVE.frame_size  = 4 ;
+  MESS_SET_SLAVE.frame_type  = STANDART_LIN_FRAME_TYPE;
+  MESS_SET_SLAVE.frame_delay  = 1920 ;
+  MESS_SET_SLAVE.frame_data  = Buf_SET_SLAVE;
+
+  //SET SLAVE FRAME
+  MESS_SET_SLAVE2.frame_id    = 0x01;
+  MESS_SET_SLAVE2.frame_size  = 4 ;
+  MESS_SET_SLAVE2.frame_type  = STANDART_LIN_FRAME_TYPE;
+  MESS_SET_SLAVE2.frame_delay  = 1920 ;
+  MESS_SET_SLAVE2.frame_data  = Buf_SET_SLAVE2;
+
+  // FRAME parameter
+  number_of_frame = 2;
+  my_schedule.frame_message[0] = MESS_SET_SLAVE;
+  my_schedule.frame_message[1] = MESS_SET_SLAVE2;
+  my_schedule.number_of_frame = number_of_frame;
 
   //CONFIG_IO_PORTS();
 
+  // Initialise LIN Controller
+  lin_init();// Performs Initialisation of LIN Software Driver
+
   sei(); 		/* Interrupts globally enabled */
 
+  // CAN interface Init
   mcp2515_init();
+
+  // Display panel Init
   dpy_trm_s01__Init();
 
   while(1) {
     // CAN message receive
 	can_receive_message(&p_message);
-
+	
+	// A kormányról érkezett üzenet a CAN buszon?
 	if(p_message.id == 0x100){	
 		switch (p_message.data[4]) {
+			// bal elsõ ajtó -> nem ez a modul kezeli
 			case (1): {active = 0; DPY_TRM_S01__LED_2_OFF(); DPY_TRM_S01__LED_1_OFF();break;}
+			// jobb elsõ ajtó -> nem ez a modul kezeli
 			case (2): {active = 0; DPY_TRM_S01__LED_2_OFF(); DPY_TRM_S01__LED_1_OFF();break;}
+			// bal hátsó ajtó aktív -> üzenetet majd tovább kell küldeni LIN buszon
 			case (4): {active = 1; DPY_TRM_S01__LED_1_ON(); DPY_TRM_S01__LED_2_OFF(); break;}
+			// jobb hátsó ajtó aktív -> üzenetet majd tovább kell küldeni LIN buszon
 			case (8): {active = 2; DPY_TRM_S01__LED_2_ON(); DPY_TRM_S01__LED_1_OFF(); break;}	
+		} // end case
+
+		// LIN_SLAVE-nek továbbküldjük az adatot, ha õt szólítottuk meg
+		if(active == 1) {
+			Buf_SET_SLAVE[0] = p_message.data[4];
+			dpy_trm_s01__7seq_write_number(Buf_SET_SLAVE[0],0);
 		}
+
+		// LIN_SLAVE2-nek továbbküldjük az adatot, ha õt szólítottuk meg
+		else if(active == 2) {
+			Buf_SET_SLAVE2[0] = p_message.data[4];
+			dpy_trm_s01__7seq_write_number(Buf_SET_SLAVE2[0],0);
+		} //if active end
+
 
 	} //if p_message.id end
 	_delay_ms(10);
